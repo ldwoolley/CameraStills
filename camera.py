9ceedf07db6478e3a22681078885bd584228be8a
Lance Woolley <ldwoolley@gmail.com>
2016-05-10 11:39:31 -0700

    First version operational.  Ready for field testing.

--- camera.py
+++ camera.py
@@ -1,21 +1,23 @@
 #!/user/bin/python
 import time
+import sys
 from shutil import disk_usage
 
 import picamera
 from time import sleep, strftime
 
-filePath = '/home/pi/camera'  # do not include the final "/"
-imageInterval = 600           # in seconds
-MAX_IMAGE_COUNT = 10000
-MAX_IMAGE_TIMER = 60*60*24
+filePath = '/home/pi/camera/'  # do not include the final "/"
+imageInterval = 10          # in seconds
+MAX_IMAGE_COUNT = 5
+MAX_IMAGE_TIMER = 60*60*24*30
 MIN_DISK_FREE_PCT = 0.1
 cameraInitErrors = 0
 cameraCaptureErrors = 0
 dateText = ""
 sensorTime = 4
+jobText = "Bird Watch"
 
-def cameraInit(self, camera ):     # Set the camera instant defaults
+def cameraInit( camera ):     # Set the camera instant defaults
     try:
         # set all the default camera settings to reduce surprises.
         camera.sharpness = 0
@@ -37,38 +39,35 @@ def cameraInit(self, camera ):     # Set the camera instant defaults
         camera.rotation = 0               # 0, 90, 180, 270
         camera.resolution = (1920, 1080)  # 64x64 to 2592x1944
         # return something if the code ran clean.
+        print('Camera initialized')
         return True
     except:
-        # count up the number of errors that have occured initializing the camera.
-        self.cameraInitErrors += 1
-        print('Sorry! but there have been %i errors initializing the camera') % self.cameraInitErrors
+        print('An exception occured during camera initialization.')
         # rase the exception so that we can keep track of what really happenedraise
         raise
         # return boolean FALSE if the code failed to initialize correctly.
         return False
-    
 
-def cameraCapture(camera, sensorTime, JobText, DateText):
+def cameraCapture( Camera, SensorTime, JobText, DateText, FilePath):
     try:
         # set Project Specific Code
-        camera.annotate_text = "%s %s" % JobText, DateText
-        camera.start_preview(alpha=255)  # between 0 and 255
-        sleep(sensorTime)
-        camera.capture('%s/image-%s.jpg') % filePath, dateText 
-        camera.stop_preview()
+        Camera.annotate_text = (JobText + " " + DateText)
+        #print('The image was annotated with %s' % (camera.annotate_text) )
+        Camera.start_preview(alpha=100)  # between 0 and 255
+        sleep(SensorTime)
+        Camera.capture(FilePath + DateText+'.jpg') 
+        Camera.stop_preview()
         # return something if the code ran clean.
         return True
+    
     except:
-        # count up the number of errors that have occured.
-        self.cameraCaptureErrors += 1
-        print('Sorry! there have been %i errors capturing images with the camera') 
-              % self.cameraInitErrors
+        print('An exception occured during image capture.')
         # rase the exception so that we can keep track of what really happenedraise
         raise
         # return boolean FALSE if the code failed to initialize correctly.
         return False
 
-def captureDateTime(interval, dateText):
+def captureDateTime(interval):
     try:
         # create the datetime label for the image based on interval in (s)
         if (interval < 60):
@@ -83,67 +82,88 @@ def captureDateTime(interval, dateText):
             dateText = strftime('%Y-%m')
         else:
             dateText = strftime('%Y')
-     
-        return True
+        return dateText
+    
     except:
-        # count up the number of errors that have occured.
-        self.dateErrors += 1
-        print('Sorry! there have been %i errors setting the date.') 
-              % self.dateErrors
+        print('An exception occured during DateTime setting.')
         # rase the exception so that we can keep track of what really happenedraise
         raise
         # return boolean FALSE if the code failed to initialize correctly.
         return False
         
-
 def main():
     errorCounter = 0
     imageCounter = 0
     imageTimer = 0
+    diskFreePct = -999
     # Handle Errors and keep running.
     try:
         # Declare a camera
         camera = picamera.PiCamera()
         
         # Setup the camera
-        startTime = time.clock()
-        nextTime = startTime + imageInterval
+        startTime = time.time()
+        nextTime = startTime
         cameraInit(camera)
+        dateText = captureDateTime(imageInterval)
+        print('The images will be annoated with %s.' % (jobText + ' ' + dateText) )
+        print('The images will be stored with the pattern %s.' % (filePath + dateText + '.jpg') )
         # Sequence until max number of images or max time reached
-        while (imageTimer < MAX_IMAGE_TIMER && imageCounter < MAX_IMAGE_COUNT):
+        while (imageTimer < MAX_IMAGE_TIMER and imageCounter < MAX_IMAGE_COUNT):
             # Check disk usage and if acceptable before capturing an image.
             diskFreePct = disk_usage(filePath).free / disk_usage(filePath).total
             if ( diskFreePct > MIN_DISK_FREE_PCT):
-                # Collect the 
-                if( captureDateTime(imageInterval, dateText) ):
-                    pass
+                # Collect the date and time
+                dateText = captureDateTime(imageInterval)
+                if( len(dateText) > 1 ):
+                    if( cameraCapture ( camera, sensorTime, jobText, dateText, filePath ) ):
+                        imageCounter += 1
+                    else:
+                        print('The camera capture failed for some reason')
+                        break
                 else:
                     print('The time date failed for some reason')
-                if( cameraCapture ( camera, sleepTime, JobText, DateText ) ):
-                    imageCounter++
-                else:
-                    print('The camera capture failed for some reason')
+                    break
             else:
-                print('The disk space is nearly full, %s MB free space') 
-                           % str(disk_usage(filePath)/1024/1024)
+                print('The disk space is nearly full:', 
+                       disk_usage(filePath)/1024/1024 ,
+                      'MB free space')
                 break
-            # Calculate the sleep to take the next picture on time.
-            sleep( nextTime-time.clock)
+
+            #sleep( nextTime-time.time() )
             nextTime = nextTime + imageInterval
+            while nextTime > time.time():
+                message = 'Captured {0:5d} images. Next image in {1:5.0f} s.'.format(imageCounter, (nextTime - time.time()))
+                print( message, end='\r', flush=True )
+                sleep(1)
+            #print('\n')
+            # Calculate the sleep to take the next picture on time.
+            #sleepTime = nextTime - time.time()
+            #print('The next time will be %i ' % (nextTime) )
+            #print('The capture interval is %i ' % (imageInterval) )
+            #print('the current time is %i ' % ( time.time() ) )
+            #print('The sleep time will be %i .' % (sleepTime) )
+            #print('The new next time will be %i ' % (nextTime + imageInterval ) )
+        print('\n')
+        print('Program has completed running')
         
-    except:
-        print("We have encountered %i errros") % ++ErrorCount
+    except (KeyboardInterrupt, SystemExit):
+        camera.close()
+        print('You provided a KeyboardInterrupt or computer sent a SystemExit and the program will clean up.')
         raise
-        if ErrorCount > ErrorLimit:
-            break
+
+    except:
+        camera.close()
+        print('The program encountered and unkown exception, and will clean up.')
+        raise
 
     finally:
         # close up the camera functions before exiting
         camera.close()
         # output basic parameters
-        print('System ran for %s seconds') % str(imageTimer)
-        print('System took %s images') % str(imageCounter)
-        print('System free disk space is %s %') % str(diskFreePct)
+        print('Program ran for %i minuts.' % ((time.time() - startTime)/60) )
+        print('Program captured %i images.' % (imageCounter) ) 
+        print('System free disk space stands at %2.0f percent.' % (diskFreePct*100) )
 
 if __name__ == "__main__":
     main()
